19eaf94c85e2d8b45e925de7a3f0c574bbf788bd
stevenjmarsh <sjm.atl@gmail.com>
2018-11-21 13:02:40 -0500

    moving header and logo components to a header folder

--- imports/ui/components/Header/tests/__stories__/Header.stories.jsx
+++ imports/ui/components/Header/tests/__stories__/Header.stories.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Header from "../../Header";
+import Header from "../..";
 
 storiesOf("Components", module).add("Header", () => <Header />);
