9e2f499476e3172645f6346f04e6caedd90a2b50
stevenjmarsh <sjm.atl@gmail.com>
2018-11-21 16:55:55 -0500

    adding router, and initial route

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable arrow-parens, object-curly-newline */
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Content from "./components/Content";
 
@@ -9,10 +10,13 @@ export default class App extends Component {
 
   render() {
     return (
-      <Fragment>
-        <Header />
-        <Content />
-      </Fragment>
+      <Router>
+        <div>
+          <Header />
+
+          <Route exact path="/" component={Content} />
+        </div>
+      </Router>
     );
   }
 }
