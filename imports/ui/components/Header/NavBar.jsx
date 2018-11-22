6be1a9136d82200e223ce05fbdec3b4e1ee5618b
stevenjmarsh <sjm.atl@gmail.com>
2018-11-22 10:57:57 -0500

    adding link styling to NavBar

--- imports/ui/components/Header/NavBar.jsx
+++ imports/ui/components/Header/NavBar.jsx
@@ -3,15 +3,21 @@ import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 
-const styles = {
+const styles = theme => ({
   container: {
     // margin: "5px 0 5px 0",
   },
   selected: {
-    color: "red",
+    color: theme.palette.primary.main,
+    "&:hover": {
+      color: theme.palette.primary.main,
+    },
   },
-};
+});
 
+// TODO: to set an anchor link active, could check location.hash
+// if equal, set Contact to active
+// Note, may need to use Link instead of NavLink to do so
 const NavBar = ({ classes: { container, selected } }) => (
   <nav className={container}>
     <ul>
