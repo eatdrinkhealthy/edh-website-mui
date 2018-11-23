561666ccbcfc00389139a4c4c47070801ee467ae
stevenjmarsh <sjm.atl@gmail.com>
2018-11-23 17:12:04 -0500

    adding banner button style

--- imports/ui/components/Content/Banner.jsx
+++ imports/ui/components/Content/Banner.jsx
@@ -1,29 +1,45 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
 
-const Banner = () => (
-  <header className="app-header">
-    <section className="app-header__banner">
-      <div className="app-header__banner-content-wrapper">
-        <div className="row">
-          <div className="column medium-8 large-7">
-            <div className="app-header__banner-headline">
-              A platform to find, share, and discuss healthy places to shop and eat.
-            </div>
-            <div className="app-header__banner-buttons">
-              <a
-                href="https://app.eatdrinkhealthy.co/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="button button--wep-app"
-              >
-                <span>Web App</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </header>
+/* eslint-disable jsx-a11y/anchor-has-content */
+
+const MyLink = props => (
+  <a
+    href="https://app.eatdrinkhealthy.co/"
+    target="_blank"
+    rel="noopener noreferrer"
+    {...props}
+  />
 );
 
-export default Banner;
+const styles = {
+  bannerButton: {
+    color: "white",
+    fontWeight: "bold",
+    width: "140px",
+  },
+};
+
+const Banner = ({ classes: { bannerButton } }) => (
+  <section className="banner-container">
+    <div className="banner-headline">
+      A platform to find, share, and discuss healthy places to shop and eat.
+    </div>
+    <Button
+      variant="extendedFab"
+      color="primary"
+      className={bannerButton}
+      component={MyLink}
+    >
+      Web App
+    </Button>
+  </section>
+);
+
+Banner.propTypes = {
+  classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+
+export default withStyles(styles)(Banner);
