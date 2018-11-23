a8870ab651621be0794d22a1074e8b6be7c196b2
stevenjmarsh <sjm.atl@gmail.com>
2018-11-23 18:23:17 -0500

    creating placeholders for Contact Image and Form components

--- imports/ui/components/Content/Contact.jsx
+++ imports/ui/components/Content/Contact.jsx
@@ -3,22 +3,37 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = {
-  section: {
+  container: {
     height: "400px",
     border: "2px solid brown",
     margin: "5px",
     padding: "5px",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  image: {
+    border: "1px dashed blue",
+    width: "80px",
+    textAlign: "center",
+  },
+  form: {
+    border: "1px dashed green",
+    width: "80px",
+    textAlign: "center",
   },
 };
 
 const Contact = props => {
   const {
-    classes: { section },
+    classes: { container, image, form },
   } = props;
 
   return (
-    <div id="contact" className={section}>
-      Contact
+    <div id="contact" className={container}>
+      <span className={image}>Image</span>
+      <span className={form}>Form</span>
     </div>
   );
 };
